15397373e19385503fe3ace7b55a0fbe8c830673
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-01-22 00:35:35 -0500

    List and conditions

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 26}
+      { id: 'wer1', name: 'Max', age: 28},
+      { id: 'wer2', name: 'Manu', age: 29},
+      { id: 'wer3', name: 'Stephanie', age: 26}
     ], 
-    otherState: 'something'
+    otherState: 'something',
+    showPersons: false
   }
 
   switchHandler = (newName) => {
@@ -17,22 +18,38 @@ class App extends Component {
     
     this.setState({
       persons: [
-        { name: newName, age: 28},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 27}
+        { id: 'wer1', name: newName, age: 28},
+        { id: 'wer2', name: 'Manu', age: 29},
+        { id: 'wer3', name: 'Stephanie', age: 27}
       ]
     });
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28},
-        { name: event.target.value, age: 29},
-        { name: 'Stephanie', age: 27}
-      ],
-      showPersons: false
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -55,17 +72,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}/>
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchHandler.bind(this, 'Maxi!')}
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
+          {this.state.persons.map((person, index) => {
+            return <Person 
+                      key={person.id}
+                      name={person.name} 
+                      age={person.age}
+                      click={this.deletePersonHandler.bind(this, index)}
+                      changed={(event) => {this.nameChangedHandler(event, person.id)}} />
+          })}
+            
           </div>
       );
     }
