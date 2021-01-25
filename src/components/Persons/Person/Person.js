73daed1d3221034a851c784c65f11c6dadac2a43
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-01-25 00:50:20 -0500

    withClass hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 
 class Person extends Component {
@@ -7,13 +8,13 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-        <Fragment>
+        <Aux>
             <div className={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
             </div>
-        </Fragment>
+        </Aux>
         );
     }
 }
