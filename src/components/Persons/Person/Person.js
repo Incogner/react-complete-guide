596f642f5be49c8c157160ecc95d341b92efb8e1
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-01-26 21:17:12 -0500

    passing unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component {
@@ -9,14 +10,12 @@ class Person extends Component {
         console.log('[Person.js] rendering...');
         return (
         <Aux>
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name} />
         </Aux>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
